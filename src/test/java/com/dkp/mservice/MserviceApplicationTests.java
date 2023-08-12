6164781f3a795a6006f53f603095bac5d0e3318c
dkp1903 <dushyantpathak6789@gmail.com>
2023-08-12 16:18:33 +0000

    initiate

---
+++ src/test/java/com/dkp/mservice/MserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkp.mservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
